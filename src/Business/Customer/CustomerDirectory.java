1d392d7c451058f7a0859cc940cf56d1041d7472
YU AN <ayaaylovelife@gmail.com>
2021-11-15 14:43:09 -0500

    first commit

---
+++ src/Business/Customer/CustomerDirectory.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Customer;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author harold
+ */
+public class CustomerDirectory {
+    private ArrayList<Customer> customer ;
+
+    public ArrayList<Customer> getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(ArrayList<Customer> customer) {
+        this.customer = customer;
+    }
+   
+}
